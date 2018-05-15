e446585bb777f2fc12b9ddf4083698a69f17c983
Angelo Gazzola <angelo@nglgzz.com>
2018-05-15 11:18:43 +0200

    add mouse layer

--- firmware/keymap_42.c
+++ firmware/keymap_42.c
@@ -2,21 +2,27 @@
 
 const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KEYMAP(\
-      TAB, Q, W, E, R, T,                Y, U, I, O, P, FN22,\
+      TAB, Q, W, E, R, T,                Y, U, I, O, P, BSPC,\
       FN0, A, S, D, F, G,                H, J, K, L, SCLN, FN1, \
       LSFT, FN3, X, C, V, B,              N, M, COMM, DOT, FN4, FN5, \
-                      FN6, FN7,          FN7, FN8, \
-                               FN9,  FN10),
+                      FN6, FN7,          FN8, FN9, \
+                               FN10,  FN11),
     KEYMAP(\
       GRAVE, 1, 2, 3, 4, 5,                          6, 7, 8, 9, 0, EQUAL,\
-      INS, FN11, FN12, FN13, FN14, FN15,           FN16, FN17, FN18, FN19, FN20, MINUS, \
+      INS, FN21, FN22, FN23, FN24, FN25,           FN26, FN27, FN28, FN29, FN30, MINUS, \
       F1, F2, F3, F4, F5, F6,                       F7, F8, F9, F10, F11, F12, \
                               TRNS, TRNS,           TRNS, TRNS, \
                                          TRNS, TRNS),
     KEYMAP(\
       TRNS, TRNS, TRNS, PGUP, TRNS, TRNS,           TRNS, TRNS, UP, TRNS, TRNS, TRNS,\
-      TRNS, TRNS, HOME, PGDOWN, END, TRNS,          FN21, LEFT, DOWN, RIGHT, TRNS, TRNS, \
+      TRNS, TRNS, HOME, PGDOWN, END, TRNS,          FN12, LEFT, DOWN, RIGHT, TRNS, TRNS, \
       TRNS, TRNS, MSTP, MPLY, MPRV, MNXT,           _VOLDOWN, _VOLUP, _MUTE, TRNS, TRNS, TRNS, \
+                              TRNS, TRNS,           TRNS, TRNS, \
+                                         TRNS, TRNS),
+    KEYMAP(\
+      TRNS, TRNS, WH_U, TRNS, TRNS, TRNS,           TRNS, BTN1, MS_U, BTN2, TRNS, TRNS,\
+      TRNS, WH_L, WH_D, WH_R, TRNS, TRNS,           TRNS, MS_L, MS_D, MS_R, TRNS, TRNS, \
+      TRNS, TRNS, TRNS, TRNS, TRNS, TRNS,           TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, \
                               TRNS, TRNS,           TRNS, TRNS, \
                                          TRNS, TRNS),
     KEYMAP(\
@@ -34,26 +40,27 @@ const action_t PROGMEM fn_actions[] = {
   [5] = ACTION_MODS_TAP_KEY(MOD_LSFT, KC_DELETE),
 
   [6] = ACTION_LAYER_TAP_KEY(1, KC_LBRACKET),
-  [7] = ACTION_LAYER_TAP_KEY(2, KC_SPACE),
-  [8] = ACTION_LAYER_TAP_KEY(1, KC_RBRACKET),
-  [9] = ACTION_MODS_TAP_KEY(MOD_LGUI, KC_QUOTE),
-  [10] = ACTION_MODS_TAP_KEY(MOD_LGUI, KC_BSLASH),
-
-  [11] = ACTION_MODS_KEY(MOD_LSFT, KC_1),
-  [12] = ACTION_MODS_KEY(MOD_LSFT, KC_2),
-  [13] = ACTION_MODS_KEY(MOD_LSFT, KC_3),
-  [14] = ACTION_MODS_KEY(MOD_LSFT, KC_4),
-  [15] = ACTION_MODS_KEY(MOD_LSFT, KC_5),
-  [16] = ACTION_MODS_KEY(MOD_LSFT, KC_6),
-  [17] = ACTION_MODS_KEY(MOD_LSFT, KC_7),
-  [18] = ACTION_MODS_KEY(MOD_LSFT, KC_8),
-  [19] = ACTION_MODS_KEY(MOD_LSFT, KC_9),
-  [20] = ACTION_MODS_KEY(MOD_LSFT, KC_0),
+  [7] = ACTION_LAYER_TAP_KEY(3, KC_SPACE),
+  [8] = ACTION_LAYER_TAP_KEY(2, KC_SPACE),
+  [9] = ACTION_LAYER_TAP_KEY(1, KC_RBRACKET),
+  [10] = ACTION_MODS_TAP_KEY(MOD_LGUI, KC_QUOTE),
+  [11] = ACTION_MODS_TAP_KEY(MOD_LGUI, KC_BSLASH),
 
   // Mod + A (help)
-  [21] = ACTION_MODS_KEY(MOD_LGUI, KC_A),
+  [12] = ACTION_MODS_KEY(MOD_LGUI, KC_A),
   // Ctrl + Backspace (hello Satan)
-  [22] = ACTION_MODS_KEY(MOD_LCTL, KC_BSPACE),
+  [13] = ACTION_MODS_KEY(MOD_LCTL, KC_BSPACE),
+
+  [21] = ACTION_MODS_KEY(MOD_LSFT, KC_1),
+  [22] = ACTION_MODS_KEY(MOD_LSFT, KC_2),
+  [23] = ACTION_MODS_KEY(MOD_LSFT, KC_3),
+  [24] = ACTION_MODS_KEY(MOD_LSFT, KC_4),
+  [25] = ACTION_MODS_KEY(MOD_LSFT, KC_5),
+  [26] = ACTION_MODS_KEY(MOD_LSFT, KC_6),
+  [27] = ACTION_MODS_KEY(MOD_LSFT, KC_7),
+  [28] = ACTION_MODS_KEY(MOD_LSFT, KC_8),
+  [29] = ACTION_MODS_KEY(MOD_LSFT, KC_9),
+  [30] = ACTION_MODS_KEY(MOD_LSFT, KC_0),
 };
 
 /*
