07ef6ecddca88912c352bfbddb7381b66c2969b1
Angelo Gazzola <angelo@nglgzz.com>
2021-10-21 22:15:40 +0200

    Update firmware makefile, remove QMK git module

--- firmware/rules.mk
+++ firmware/rules.mk
@@ -1,11 +1,8 @@
 # MCU name
 MCU = atmega32u4
 
-# Processor frequency
-F_CPU = 8000000
-
 # Bootloader selection
-BOOTLOADER = caterina
+BOOTLOADER = atmel-dfu
 
 # Build Options
 #   change yes to no to disable
