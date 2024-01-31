e5ba388f974332a0d46c05d11143645bd0628d5b
Angelo Gazzola <angelo@nglgzz.com>
2024-01-31 14:19:30 +0100

    Update firmware

--- firmware/config.h
+++ firmware/config.h
@@ -1,41 +1,6 @@
 #pragma once
 
-#include "config_common.h"
-
-/* USB Device descriptor parameter */
-#define VENDOR_ID       0xFEED
-#define PRODUCT_ID      0x0000
-#define DEVICE_VER      0x0003
-#define MANUFACTURER    nglgzz
-#define PRODUCT         42
-
-/* key matrix size */
-#define MATRIX_ROWS 4
-#define MATRIX_COLS 12
-
-/*
- * Keyboard Matrix Assignments
- *
- * Change this to how you wired your keyboard
- * COLS: AVR pins used for columns, left to right
- * ROWS: AVR pins used for rows, top to bottom
- * DIODE_DIRECTION: COL2ROW = COL = Anode (+), ROW = Cathode (-, marked on diode)
- *                  ROW2COL = ROW = Anode (+), COL = Cathode (-, marked on diode)
- *
-*/
-#define MATRIX_COL_PINS { D1, D0, D4, C6, D7, E6, B3, B1, F7, F6, F5, F4 }
-#define MATRIX_ROW_PINS { B4, B5, B2, B6 }
-#define UNUSED_PINS
-
-/* COL2ROW, ROW2COL */
-#define DIODE_DIRECTION COL2ROW
-
-/* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
-#define DEBOUNCE 5
-
 /* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
 #define LOCKING_SUPPORT_ENABLE
 /* Locking resynchronize hack */
 #define LOCKING_RESYNC_ENABLE
-
-#define TAPPING_TERM 150
--- firmware/keymaps/default/keymap.c
+++ firmware/keymaps/default/keymap.c
@@ -15,6 +15,9 @@
 #define L1_LBRC LT(1, KC_LBRC)
 #define L1_RBRC LT(1, KC_RBRC)
 
+// One Shot Keys
+#define OS_RALT OSM(MOD_RALT)
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // Default Layer
   LAYOUT(
@@ -28,13 +31,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_GRV , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,       KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_EQL ,
     KC_INS , S(KC_1), S(KC_2), S(KC_3), S(KC_4), S(KC_5),       S(KC_6), S(KC_7), S(KC_8), S(KC_9), S(KC_0), KC_MINS,
     KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  ,       KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 , KC_F12 ,
-                               KC_CAPS, _______, _______,       _______, _______, _______
+                               KC_CAPS, OS_RALT, _______,       _______, _______, _______
   ),
+
   // Navigation Layer
   LAYOUT(
     _______, _______, _______, KC_PGUP, _______, _______,       KC_BRIU, _______, KC_UP  , _______, _______, _______,
     _______, _______, KC_HOME, KC_PGDN, KC_END , _______,       _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______,
-    _______, _______, _______, _______, _______, _______,       KC_BRID, KC_VOLD, KC_MUTE, KC_VOLU, _______, _______,
+    _______, _______, KC_MPRV, KC_MPLY, KC_MNXT, _______,       KC_BRID, KC_VOLD, KC_MUTE, KC_VOLU, _______, _______,
                                _______, _______, _______,       _______, _______, _______
   ),
   // Mouse Layer
